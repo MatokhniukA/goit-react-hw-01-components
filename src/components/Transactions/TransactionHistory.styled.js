463ab193329d60dcd1beec32dc046134bf772c7d
MaksymchukA <matokhniuk.ao@gmail.com>
2023-03-07 09:57:32 +0200

    did the first homework

--- src/components/Transactions/TransactionHistory.styled.js
+++ src/components/Transactions/TransactionHistory.styled.js
@@ -1,1 +1,18 @@
-import { styled } from 'styled-components';
+import styled from 'styled-components';
+
+export const TableContainer = styled.table`
+  width: 245px;
+  text-align: center;
+  border: 1px solid black;
+  width: 275px;
+  margin: 0 auto 15px;
+`;
+
+export const TableHead = styled.thead`
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 36px;
+  text-align: center;
+  color: white;
+  background-color: blue;
+`;
